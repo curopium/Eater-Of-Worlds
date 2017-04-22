3e436d21d024d19584a1433bd5da3e93a724516c
Ryan <kobitoko@gmail.com>
2017-04-22 16:05:47 -0600

    camera and movement with acceleration

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public float padding;
+    private bool hit;
+
+	// Use this for initialization
+	void Start () {
+        GameObject worm = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerLoc = worm.GetComponent<Transform>().position;
+        playerLoc.z = this.transform.position.z;
+        this.transform.position = playerLoc;
+        float height = 2 * Camera.main.orthographicSize - padding;
+        float width = Camera.main.aspect * height;
+        this.GetComponent<BoxCollider2D>().size = new Vector2(width, height);
+        hit = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Transform ct = this.transform;
+        GameObject worm = GameObject.FindGameObjectWithTag("Player");
+        Worm_Head wormHead = worm.GetComponent<Worm_Head>();
+        Vector3 playerLoc = worm.GetComponent<Transform>().position;
+        playerLoc.z = ct.position.z;
+        if(hit)
+            ct.position = Vector3.MoveTowards(ct.position, playerLoc, wormHead.currentSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            hit = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+            hit = true;
+    }
+}
--- Assets/Scripts/Worm_Head.cs
+++ Assets/Scripts/Worm_Head.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class Worm_Head : MonoBehaviour {
 
-    public float speed;
+    public float acceleration;
     public float turn_rate;
+    public float maxSpeed;
+    public float currentSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+        currentSpeed = 0f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    
+    // Update is called once per frame
+    void Update () {
         // Player Control
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
+        // Forwards
         if (v > 0f)
         {
-            transform.Translate(v * Vector3.up * speed * Time.deltaTime);
+            currentSpeed = Mathf.Min(maxSpeed, currentSpeed + v * acceleration);
         }
+        else
+        {
+            currentSpeed = Mathf.Max(0f, currentSpeed - acceleration);
+        }
+        // Slide around in space? Space is ice? Ice Space!
+        transform.Translate(Vector3.up * currentSpeed * Time.deltaTime);
+
+        // Turning
         if (h < 0f)
         {
             transform.Rotate(-h * Vector3.forward * -turn_rate * Time.deltaTime);
