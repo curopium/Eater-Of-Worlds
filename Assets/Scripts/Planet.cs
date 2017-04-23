9fc56d1d4cc5bf6f190b163c2116e640d652d82f
Thomas Curnow <curnow@ualberta.ca>
2017-04-22 18:16:02 -0600

    Changed it up to exploding space rocks

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Planet : MonoBehaviour {
+
+    public GameObject broken_piece;
+    public int number_pieces;
+    public float radius;
+
+	// Use this for initialization
+	void Start () {
+        radius = gameObject.GetComponent<CircleCollider2D>().radius;
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDestroy ()
+    {
+        for(int i = 0; i < number_pieces; i++)
+        {   
+            Vector3 random_point = Random.insideUnitCircle * radius;
+            random_point.x += transform.position.x;
+            random_point.y += transform.position.y;
+            random_point.z += transform.position.z;
+
+   
+
+            GameObject clone = Instantiate(broken_piece, random_point, Quaternion.Euler(new Vector3(0, 0, Random.Range(-180, 180))));
+            clone.SetActive(true);
+        }
+    }
+}
