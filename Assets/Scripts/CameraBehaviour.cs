ba6b3939a4201b34af5d84946fb701c8f35a8571
Ryan <kobitoko@gmail.com>
2017-04-23 01:22:04 -0600

    shoulda just done this. wow im restarded, i put coroutine while loop checking for time before.

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -6,9 +6,10 @@ public class CameraBehaviour : MonoBehaviour {
 
     public float padding;
     private bool hit;
+    private float oldTime;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         GameObject worm = GameObject.FindGameObjectWithTag("Player");
         Vector3 playerLoc = worm.GetComponent<Transform>().position;
         playerLoc.z = this.transform.position.z;
@@ -17,6 +18,7 @@ public class CameraBehaviour : MonoBehaviour {
         float width = Camera.main.aspect * height;
         this.GetComponent<BoxCollider2D>().size = new Vector2(width, height);
         hit = false;
+        oldTime = 0f;
     }
 	
 	// Update is called once per frame
@@ -29,19 +31,28 @@ public class CameraBehaviour : MonoBehaviour {
         if(hit)
             ct.position = Vector3.MoveTowards(ct.position, playerLoc, wormHead.currentSpeed * Time.deltaTime);
         // TO TEST ScreenShake
-        if (Input.GetKey(KeyCode.Q))
-            screenShake();
+        if (Input.GetKeyDown(KeyCode.Q))
+            screenShake(1.5f);
+        actualShake();
     }
 
-    public void screenShake()
+    public void screenShake(float duration)
+    {
+        // in seconds
+        oldTime = Time.time + duration;
+    }
+    private void actualShake()
     {
         // 0.2f is quite strong
-        float str = 0.1f;
-        float x = Random.Range(-str, str);
-        float y = Random.Range(-str, str);
-        this.transform.position += new Vector3(x, y, 0);
+        if (oldTime >= Time.time)
+        {
+            float str = 0.1f;
+            float x = Random.Range(-str, str);
+            float y = Random.Range(-str, str);
+            this.transform.position += new Vector3(x, y, 0);
+        }
     }
-
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
