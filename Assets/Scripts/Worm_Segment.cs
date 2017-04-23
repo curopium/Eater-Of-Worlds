4e8d3c6c11e8f41db699d361deab5a5a33fa7298
Thomas Curnow <curnow@ualberta.ca>
2017-04-23 17:31:59 -0600

    worm stuff

--- Assets/Scripts/Worm_Segment.cs
+++ Assets/Scripts/Worm_Segment.cs
@@ -6,6 +6,7 @@ public class Worm_Segment : MonoBehaviour
 {
 
     public GameObject parent;
+    public bool tail;
 
     // Use this for initialization
     void Start()
@@ -20,16 +21,31 @@ public class Worm_Segment : MonoBehaviour
 
         //translation
         //http://answers.unity3d.com/questions/390009/3d-snake-classical-movement-1.html
+
         Vector3 vector_From_Parent = transform.position - parent.transform.position;
-        vector_From_Parent = vector_From_Parent.normalized * transform.localScale.y;
+        //Dont need anything with the Z
+        vector_From_Parent.z = 0;
+        //*2 is a hack
+        if (!tail)
+        {
+            vector_From_Parent = vector_From_Parent.normalized * transform.localScale.y * 4;
+        } else
+        {
+            vector_From_Parent = vector_From_Parent.normalized * transform.localScale.y * 12;
+        }
         transform.position = vector_From_Parent + parent.transform.position;
+
+        Vector3 layerSorter = transform.position;
+        layerSorter.z = layerSorter.z + 1;
+        transform.position = layerSorter;
+
+        //get the layer right
 
         //rotation
         Vector3 dir = parent.transform.position - transform.position;
         //the -90 degrees is a lazy hack so I dont have to change placeholder sprites
         float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) - 90;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
     }
 
     public void takeDamage(float dam)
