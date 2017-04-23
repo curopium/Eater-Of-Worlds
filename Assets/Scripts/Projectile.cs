40283d8c11ebdcd2637d26ed53ef5dacf4f46d9e
curopium <curnow@ualberta.ca>
2017-04-22 21:10:20 -0600

    added spaceship

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+    public float damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * Vector3.up * speed * Time.deltaTime);
+
+    }
+}
